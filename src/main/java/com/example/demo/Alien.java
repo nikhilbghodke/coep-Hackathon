0f728a0957b0e021602ca3b3631abe0129fb1915
Nikhil <nikhil.ghodke17@vit.edu>
2019-09-19 20:00:46 +0530

    changed table name of database

--- src/main/java/com/example/demo/Alien.java
+++ src/main/java/com/example/demo/Alien.java
@@ -6,8 +6,10 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="ping")
 public class Alien {
 
 
